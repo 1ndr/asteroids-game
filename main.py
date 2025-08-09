b8f7a42539462d38d254336df7ce67044db2382e
1ndr <archie.jayawantha@gmail.com>
2025-08-09 20:47:11 +1000

    created movements for player

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
     dt =0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for thing in drawable: 
+            thing.draw(screen)
+
         pygame.display.flip() 
         
         # limit the framerate to 60 FPS
